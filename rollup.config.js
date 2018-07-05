e49737de01100e6079e197e9812f6d4a1d843e0d
Albin Larsson <mail@albinlarsson.com>
2018-07-05 23:53:59 +0200

    fix(package): Support both commonjs and mjs for node.js via extention-less main entry, change build

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,19 @@ import {terser} from 'rollup-plugin-terser';
 const name = 'queryStringEncode';
 const pkg = require('./package');
 const amd = {id: pkg.name};
-const input = `dist/index.js`;
+const input = 'dist/index.js';
 const banner = `/*! ${name} v${pkg.version} | (c) ${new Date().getFullYear()} ${pkg.author} | Licensed under ${pkg.license} */`;
-
 const comments = (node, {type, value}) => type === "comment2" && value.startsWith('!'); // keep banner when minifying
-const config = (format, {minify = false, isDefault = false} = {}) => {
-	const output = {name, amd, format, banner, sourcemap: minify, file: `dist/${pkg.name}${isDefault ? '' :  `.${format}`}${minify ? '.min' : ''}.js`};
-	const plugins = minify ? [terser({output: {comments}})] : [];
-	return {input, output, plugins};
-};
+const formats = {amd: 'amd.js', cjs: 'cjs.js', es: 'mjs', umd: 'js'};
 
 export default [
-	config('iife', {isDefault: true}),
-	config('iife', {isDefault: true, minify: true}),
-	config('amd'),
-	config('cjs'),
-	config('es'),
-	config('umd'),
-	config('umd', {minify: true}),
+	{
+		input,
+		output: Object.entries(formats).map(([format, ext]) => ({amd, banner, name, format, file: `dist/${pkg.name}.${ext}`})),
+	},
+	{
+		input,
+		output: {amd, banner, name, file: `dist/${pkg.name}.min.js`, format: 'umd', sourcemap: true},
+		plugins: [terser({output: {comments}})],
+	}
 ];
